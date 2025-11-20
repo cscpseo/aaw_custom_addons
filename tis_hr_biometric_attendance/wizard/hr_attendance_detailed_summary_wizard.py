8bed4471db9dc8d074d62ed04ad23cd2cfd61693
cscpeso <cscpseo>
2025-11-20 13:25:20 +0500

    Fix Attendance

--- tis_hr_biometric_attendance/wizard/hr_attendance_detailed_summary_wizard.py
+++ tis_hr_biometric_attendance/wizard/hr_attendance_detailed_summary_wizard.py
@@ -7,7 +7,7 @@ class HrAttendanceDetailedSummaryWizard(models.TransientModel):
 
     date_from = fields.Date(string="Start Date", required=True)
     date_to = fields.Date(string="End Date",   required=True)
-    company_id = fields.Many2one('res.company', string="Company")
+    company_id = fields.Many2one('res.company', string="Company", required=True)
     department_id = fields.Many2one('hr.department', string="Department", domain="[('company_id', '=', company_id)]")
     employee_id = fields.Many2one(
         'hr.employee',
